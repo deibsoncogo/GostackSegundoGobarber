6c48c48745ac9dafe7fe94ee3ba79820a8ecc1bd
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-27 16:44:55 -0300

    7.1 ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ 
-    mensagem: 'Hello Word',
-    submensagem: 'Deibson Cogo',
-  });
-});
+app.get('/', (request, response) => response.json({
+  mensagem: 'Hello Word',
+  submensagem: 'Deibson Cogo',
+}));
 
 app.listen(3333, () => {
   console.log('SERVIDOR RODANDO NA PORTA 3333');
