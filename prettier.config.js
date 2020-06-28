3ffbd910134cc7d0ac3abf386aac40e7b4bdd5c5
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-28 18:12:59 -0300

    Correção na padronização

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	singleQuote: true, // ASPAS SIMPLES
 	trailingComma: 'all', // VIRGLA NO FINAL DE OBJETOS
-	arrowParens: 'avoid', // SEM () PARA UM UNICA LINHA
+	arrowParens: 'avoid', // SEM PARENTESES PARA UM UNICA LINHA
+	printWidth: 101, // TAMANHO LIMITE DA LINHA
 };
