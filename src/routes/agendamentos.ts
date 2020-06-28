a528693bcc8553af057f7bdf57bc267a80b25e57
Deibson Cogo <deibsoncogo@outlook.com>
2020-06-28 20:57:15 -0300

    7.2 Validando a data

--- src/routes/agendamentos.ts
+++ src/routes/agendamentos.ts
@@ -1,20 +1,42 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const agendamentosRota = Router();
 
+// CRIACAO DA TIPAGEM DOS DADOS
+interface Agendamento {
+	id: string;
+	profissional: string;
+	data: Date;
+}
+
 // CRIA UM BANCO DE BADOS QUE RESETA AO REINICIAR O SERVIDOR
-const agendamentos = [];
+// VINCULANDO A TIPAGEM
+const agendamentos: Agendamento[] = [];
 
 agendamentosRota.post('/', (request, response) => {
 	// RECUPERA AS INFORMACOES NO request.body
 	const { profissional, data } = request.body;
 
-	// PERMITE A MINUPULACAO DOS DADOS ANTES DE SALVAR
+	// CONVERTE O FORMATO DO HORARIO E ARRENDONDA PARA HORA CHEIA
+	const dataConvertidaeArredondada = startOfHour(parseISO(data));
+
+	// VERIFICA SE JA EXISTE ESTE HORARIO CADASTRADO
+	const horarioDuplicado = agendamentos.find(agendamento =>
+		isEqual(dataConvertidaeArredondada, agendamento.data),
+	);
+
+	// RETORNAR UMA MENSAGEM SE A VARIAVEL POR VERDADEIRA
+	if (horarioDuplicado) {
+		return response.status(400).json({ erro: 'Horário já reservado' });
+	}
+
+	// PERMITE A MINIPULACAO DOS DADOS ANTES DE SALVAR
 	const agendamento = {
 		id: uuid(),
 		profissional,
-		data,
+		data: dataConvertidaeArredondada,
 	};
 
 	// REALIZA O SALVAMENTO NO BANCO DE DADOS TEMPORARIO
