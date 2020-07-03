390ac820805720598545347e10d94a5dce4d00ef
Deibson Cogo <deibsoncogo@outlook.com>
2020-07-03 20:21:10 -0300

    8.1 Criando model de agendamento

--- src/models/agendamento.ts
+++ src/models/agendamento.ts
@@ -1,21 +1,32 @@
 // CRIACAO DA TIPAGEM DOS DADOS GLOBALMENTE
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+// import { uuid } from 'uuidv4';
+
+// O Decorator (@) COM O Entity CRIAR O VINCULO COM O BANCO DE DADOS
+// ESTE METODO CRIA UM Constructor AUTOMATICAMENTE
+@Entity('agendamentos')
 class Agendamento {
+	// DEFINE COMO COLUNA PRINCIPAL E QUE É GERADA AUTOMATICAMENTE PELO UUID
+	@PrimaryGeneratedColumn('uuid')
 	id: string;
 
+	// DEFINE QUE E UMA COLUNA TIPO STRING
+	@Column()
 	profissional: string;
 
+	// DEFINE QUE E UMA COLUNA TIPO TIMESTAMP
+	@Column('timestamp with time zone')
 	data: Date;
 
 	// ESTE METODO SERVE PARA CRIAR NOVAS INFORMACOES
 	// Omit SERVE PARA FALAR QUE CERTO ITEM NAO E PREENCHIDO PELO USUARIO
-	constructor({ profissional, data }: Omit<Agendamento, 'id'>) {
-		// DEVEMOS DEFINIR UM VALOR DE INICIALIZACAO
-		this.id = uuid();
-		this.profissional = profissional;
-		this.data = data;
-	}
+	// constructor({ profissional, data }: Omit<Agendamento, 'id'>) {
+	// 	// DEVEMOS DEFINIR UM VALOR DE INICIALIZACAO
+	// 	this.id = uuid();
+	// 	this.profissional = profissional;
+	// 	this.data = data;
+	// }
 }
 
 export default Agendamento;
