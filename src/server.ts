0f72ba00c10bb54ad49204fe5019bb99975f8ccb
Deibson Cogo <deibsoncogo@outlook.com>
2020-07-09 20:21:55 -0300

    8.4 Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import 'reflect-metadata';
 
 import express from 'express';
-
 import rotas from './routes';
 
 import './database';
 
+import UploadConfig from './config/upload';
+
 const app = express();
 
 app.use(express.json());
-
+// PERMITE VISUALIZAR O AVATAR PELO NAVEGADOR DE FORMA ESTATICA
+app.use('/arquivos', express.static(UploadConfig.diretorio));
 app.use(rotas);
 
 app.listen(3333, () => {
