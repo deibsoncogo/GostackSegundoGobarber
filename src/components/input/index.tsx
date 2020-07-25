429936f8dbcf0e7213f5ca391e7f5860029bc795
Deibson Cogo <deibsoncogo@outlook.com>
2020-07-25 14:46:27 -0300

    10.1 Utilizando Unform

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
@@ -14,13 +15,30 @@ interface InputPropriedade extends InputHTMLAttributes<HTMLInputElement> {
 
 // O props ASSIM FAZ RECEBER TODO TIPO DE PROPRIEDADE
 // PARA ENTENDER QUE E UM COMPONENTE DEVEMOS USAR LETRA MAIUSCULA
-const Input: React.FC<InputPropriedade> = ({ icone: Icone, ...rest }) => (
-	<Container>
-		{/* REALIZA UMA VERIFICACAO SE EXISTE UM ICONE */}
-		{Icone && <Icone size={20} />}
-		{/* O props ASSIM ENVIA TODO TIPO DE PROPRIEDADE */}
-		<input {...rest} />
-	</Container>
-);
+const Input: React.FC<InputPropriedade> = ({ nome, icone: Icone, ...rest }) => {
+	const inputRef = useRef(null);
+
+	// PREPARACAO PARA CRIAR UM REGISTRO
+	const { fieldName, defaultValue, error, registerField } = useField(nome);
+
+	// CRIA UM REGISTRO QUANDO O EFFECT EXECUTAR
+	useEffect(() => {
+		registerField({
+			name: fieldName,
+			ref: inputRef.current,
+			path: 'value',
+		});
+	}, [fieldName, registerField]);
+
+	return (
+		<Container>
+			{/* REALIZA UMA VERIFICACAO SE EXISTE UM ICONE */}
+			{Icone && <Icone size={20} />}
+			{/* O props ASSIM ENVIA TODO TIPO DE PROPRIEDADE */}
+			{/* O defaultValue PERMITE INICIAR UM VALOR INICIAL NO CAMPO */}
+			<input defaultValue={defaultValue} ref={inputRef} {...rest} />
+		</Container>
+	);
+};
 
 export default Input;
--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -1,33 +1,46 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Conteudo, Background } from './styles';
 import LogoImagem from '../../assets/logo.svg';
 import InputComponente from '../../components/input';
 import ButtonComponente from '../../components/button';
 
-const Cadastro: React.FC = () => (
-	<Container>
-		<Background />
-		<Conteudo>
-			<img src={LogoImagem} alt="Logo da Gobarber" />
+const Cadastro: React.FC = () => {
+	function usuarioSubmit(data: object): void {
+		console.log(data);
+	}
 
-			<form>
-				<h1>Faça seu cadastro</h1>
+	return (
+		<Container>
+			<Background />
+			<Conteudo>
+				<img src={LogoImagem} alt="Logo da Gobarber" />
 
-				<InputComponente nome="nome" icone={FiUser} placeholder="Nome e sobrenome" />
-				<InputComponente nome="email" icone={FiMail} placeholder="E-mail" />
-				<InputComponente nome="senha" icone={FiLock} type="password" placeholder="Senha" />
+				{/* CRIA UM VALOR INICIAL DO CAMPO */}
+				<Form initialData={{ nome: 'Deibson Cogo' }} onSubmit={usuarioSubmit}>
+					<h1>Faça seu cadastro</h1>
 
-				<ButtonComponente type="submit">Cadastrar</ButtonComponente>
-			</form>
+					<InputComponente nome="nome" icone={FiUser} placeholder="Nome e sobrenome" />
+					<InputComponente nome="email" icone={FiMail} placeholder="E-mail" />
+					<InputComponente
+						nome="senha"
+						icone={FiLock}
+						type="password"
+						placeholder="Senha"
+					/>
 
-			<a href="cadastro">
-				<FiArrowLeft />
-				Voltar para logon
-			</a>
-		</Conteudo>
-	</Container>
-);
+					<ButtonComponente type="submit">Cadastrar</ButtonComponente>
+				</Form>
+
+				<a href="cadastro">
+					<FiArrowLeft />
+					Voltar para logon
+				</a>
+			</Conteudo>
+		</Container>
+	);
+};
 
 export default Cadastro;
