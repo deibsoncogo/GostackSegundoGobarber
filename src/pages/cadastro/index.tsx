c4cd770a300d6816d6a5e3df7bceaa8e017ed19a
Deibson Cogo <deibsoncogo@outlook.com>
2020-07-26 17:26:24 -0300

    10.1 Validando cadastro

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import { Container, Conteudo, Background } from './styles';
 import LogoImagem from '../../assets/logo.svg';
 import InputComponente from '../../components/input';
 import ButtonComponente from '../../components/button';
 
+// FUNCAO PARA REALIZAR A VALIDACAO DO USUARIO
 const Cadastro: React.FC = () => {
-	function usuarioSubmit(data: object): void {
-		console.log(data);
-	}
+	// FUNCAO QUE VAI EXECUTAR SOMENTE UMA FEZ
+	const usuarioSubmit = useCallback(async (data: object) => {
+		try {
+			// INFORMAMOS COMO IREMOS RECEBER OS DADOS
+			const schema = Yup.object().shape({
+				// REGRAS DE VALIDACAO
+				nome: Yup.string().required('Nome obrigatório!'),
+				email: Yup.string().required('E-mail obrigatório!').email('E-mail inválido!'),
+				senha: Yup.string().required('Senha obrigatória!').min(6, 'No mínimo 6 dígitos'),
+			});
+
+			// OQUE DEVE ACONTECER SE PASSAR PELAS REGRAS DE NEGOCIO
+			await schema.validate(data, {
+				// FAZ TRAZER TODOS OS ERROS
+				abortEarly: false,
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	}, []);
 
 	return (
 		<Container>
